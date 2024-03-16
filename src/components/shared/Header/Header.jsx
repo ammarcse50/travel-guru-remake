b57dca8dba23a6e85fe304934b49d8c7580ce25f
ammmar <ammaraslam7164@gmail.com>
2024-03-16 21:26:49 +0600

    text-white-banner

--- src/components/shared/Header/Header.jsx
+++ src/components/shared/Header/Header.jsx
@@ -9,12 +9,11 @@ import Navbar from "../Navbar/Navbar";
 const Header = () => {
   return (
     <div
-      className="hero min-h-screen "
+      className="hero min-h-screen text-white"
       style={{ backgroundImage: `url(${img1})` }}
     >
-       
       <div className="hero-overlay bg-opacity-60"></div>
-       
+
       <div className="w-1/2  ml-80">
         <div className="-ml-96 text-left">
           <h1 className="mb-5 text-5xl font-bold">COX'S BAZAR</h1>
@@ -34,41 +33,44 @@ const Header = () => {
       <div className="ml-[700px] mt-56">
         <div className="carousel w-full gap-7">
           <div id="slide1" className="carousel-item relative  ">
-            <img src={img2} className="w-[270px] h-[416px] hover:border-8   border-yellow-500 rounded-3xl " />
-          
+            <img
+              src={img2}
+              className="w-[270px] h-[416px] hover:border-8   border-yellow-500 rounded-3xl "
+            />
           </div>
           <div id="slide2" className="carousel-item relative">
-            <img src={img3} className="w-[270px] h-[416px] hover:border-8   border-yellow-500 rounded-3xl" />
-          
+            <img
+              src={img3}
+              className="w-[270px] h-[416px] hover:border-8   border-yellow-500 rounded-3xl"
+            />
           </div>
           <div id="slide3" className="carousel-item relative w-full">
-            <img src={img4} className="w-[270px] h-[416px] hover:border-8   border-yellow-500 rounded-3xl" />
-          
+            <img
+              src={img4}
+              className="w-[270px] h-[416px] hover:border-8   border-yellow-500 rounded-3xl"
+            />
           </div>
-       
         </div>
         <div className="mt-12 flex  transform -translate-y-1/2 left-5 right-5 top-1/2">
-              {/* <a href="#slide4" className="btn btn-circle">
+          {/* <a href="#slide4" className="btn btn-circle">
                 ❮
               </a>
               <a href="#slide2" className="btn btn-circle">
                 ❯
               </a>  */}
-              <a href="#slide1" className="btn bg-white text-black btn-circle">
-                ❮
-              </a>
-              <a href="#slide3" className="btn bg-white text-black btn-circle">
-                ❯
-              </a>
-              {/* <a href="#slide2" className="btn btn-circle">
+          <a href="#slide1" className="btn bg-white text-black btn-circle">
+            ❮
+          </a>
+          <a href="#slide3" className="btn bg-white text-black btn-circle">
+            ❯
+          </a>
+          {/* <a href="#slide2" className="btn btn-circle">
                 ❮
               </a>
               <a href="#slide4" className="btn btn-circle">
                 ❯
               </a> */}
-            </div>
-            
-            
+        </div>
       </div>
     </div>
   );
